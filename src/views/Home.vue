df830c25611e5aac3b6f08b6e61cc3788efbd5af
maximilianoschier <maximiliano.schier@gmail.com>
2021-09-23 17:39:47 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <form @submit.prevent="procesarFormulario">
+    <Input :tarea="tarea" />
+  </form>
+  <hr>
+  <ListaTareas />
+</template>
+
+<script>
+
+import Input from '../components/Input.vue'
+import ListaTareas from '../components/ListaTareas.vue'
+import {mapActions} from 'vuex'
+const shortid = require('shortid');
+
+export default {
+  name: 'Home',
+  components: {
+    Input, ListaTareas
+  },
+  data() {
+    return {
+      tarea: {
+        id: '',
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['setTareas', 'cargarLocalStorage']),
+    procesarFormulario() {
+      console.log(this.tarea)
+      if(this.tarea.nombre.trim() === "") {
+        console.log('Campo vacio')
+        return
+      }
+      console.log('no esta vacio')
+
+      //generar id
+      this.tarea.id = shortid.generate()
+      console.log(this.tarea.id)
+      
+      //envian los datos
+      this.setTareas(this.tarea)
+
+      //limpiar datos
+      this.tarea = {
+        id: '',
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  created() {
+    this.cargarLocalStorage()
+  }
+}
+</script>
